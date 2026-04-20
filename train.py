694aca7da266f61b6d1dfef09780bfc3cf81b615
Appointment-2024 <2593313134@qq.com>
2026-04-20 10:35:06 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,18 @@
+import warnings, os
+warnings.filterwarnings('ignore')
+from ultralytics import RTDETR
+
+if __name__ == '__main__':
+    model = RTDETR()
+    # model.load('') # loading pretrain weights
+    model.train(data='',
+                cache=False,
+                imgsz=640,
+                epochs=300,
+                batch=4,
+                workers=4,
+                # device='0,1',
+                # resume='',
+                project='',
+                name='',
+                )
